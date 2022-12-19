2faf5c7060ec4d958aeb58cc2b793d4ef29d911f
Muhammad Azeem <manixate@gmail.com>
2022-12-19 09:11:10 +0000

    Remove cheerio as it changes the output.
    Use regex for finding the tests and error expectations.

--- jest/transform.js
+++ jest/transform.js
@@ -1,7 +1,6 @@
 const path = require("path")
 const fs = require("fs")
 const crypto = require("crypto")
-const cheerio = require("cheerio")
 
 /**
  * Returns the path to index.html output file related to the test file
@@ -35,56 +34,69 @@ function findHugoTestOutputPath(testPath, hugoContentDir, hugoOutputDir) {
  */
 function generateTestCases(testPath, errors) {
   const fileData = fs.readFileSync(testPath, "utf-8")
+  const tests = {};
+  /*
+    Regex representing a test case
+    <test name="test name">
+      <!-- Test case here -->
+    </test>
+  */
+  const testCasesRegex = /<test.*?name="([^"]*?)".*?>((?:.*?\r?\n?)*?)<\/test>/gi
+  let match;
+  do {
+    match = testCasesRegex.exec(fileData);
+    if (match) {
+      tests[match[1]] = match[2];
+    }
+  } while (match);
 
-  const $ = cheerio.load(fileData, null, false)
-  const tests = $("test")
-  if (tests.length === 0) {
-    return []
-  }
+  // Create test cases
+  const generatedTestCases = []
+  for (key in tests) {
+    const value = tests[key]
+      .replace(/\\/g, '/') // Normalize file paths in errors ("folder\\file.md: ..." => "folder/file.md: ...")
+      .replace(/\r\n/g, "\\n") // CRLF on Windows with Hugo 0.60+
+      .replace(/\n/g, '\\n');
 
-  const generatedTestCases = tests
-    .map((i, test) => {
-      const testTitle = test.attribs.name
-      const value = $(test)
-        .html()
-        .replace(/[^\\]'/g, "\\'")
-        .replace(/\r\n/g, "\\n") // CRLF on Windows with Hugo 0.60+
-        .replace(/\n/g, "\\n")
-
-      const expectedError = $("div#expected-error", test)
-      if (expectedError.length > 0) {
-        // Error test case
-        const errorId = expectedError.attr("data-id")
-        const errorText = expectedError.attr("data-error")
-        const expected = errorText.replace(/'/g, "\\'")
-        // Pick the corresponding error from the logs
-        const actual = (errors.find((e) => e.expected.startsWith(`${errorId}|`)) || {}).actual
-        if (!actual) {
-          // No error raised
-          return [`it ('${testTitle}', () => {`, `  throw new Error('No error raised. Was expecting: "${expected}"');`, "})"].join("\n")
-        } else {
-          // Compare errors
-          const error = actual.replace(/'/g, "\\'")
-          return [
-            `it ('${testTitle}', () => {`,
-            `  const actual = '${error}';`,
-            `  const expected = '${expected}';`,
-            `  expect(actual).toEqual(expected);`,
-            "})"
-          ].join("\n")
-        }
+    const testTitle = key.replace(/[^\\]'/g, '\\\'')
+    /*
+      Regex representing error
+      <div id="expected-error" data-id="<error-title>" data-error="<error-text>" />
+    */
+    const errorRegex = /<div.*?id="expected-error"(?:.*?\r?\n?).*data-id="(.*)">((?:.*?\r?\n?)*?)<\/div>/gi
+    const errorMatch = errorRegex.exec(value)
+    if (errorMatch && errorMatch.length >= 2) {
+      // Error test case
+      const errorId = errorMatch[1]
+      const expected = errorMatch[2]
+      // Pick the corresponding error from the logs
+      const actual = (errors.find((e) => e.expected.startsWith(`${errorId}|`)) || {}).actual
+      if (!actual) {
+        // No error raised
+        generatedTestCases.push([
+          `it ('${testTitle}', () => {`, 
+          `  throw new Error('No error raised. Was expecting: "${expected}"');`, "})"
+        ].join("\n"))
       } else {
-        // Nominal test case, use snapshots
-        return [
+        // Compare errors
+        generatedTestCases.push([
           `it ('${testTitle}', () => {`,
-          `  const value = '${value}';`,
-          "  expect({custom: 'beautify', input: value}).toMatchSnapshot();",
+          `  const actual = \`${actual}\`;`,
+          `  const expected = \`${expected}\`;`,
+          `  expect(actual).toEqual(expected);`,
           "})"
-        ].join("\n")
+        ].join("\n"))
       }
-    })
-    .get()
-
+    } else {
+      // Nominal test case, use snapshots
+      generatedTestCases.push([
+        `it ('${testTitle}', () => {`,
+        `  const value = \`${value}\`;`,
+        "  expect({custom: 'beautify', input: value}).toMatchSnapshot();",
+        "})"
+      ].join("\n"))
+    }
+  }
   return generatedTestCases
 }
 
@@ -117,7 +129,7 @@ module.exports = {
     const hugoTestPath = findHugoTestOutputPath(filepath, jestHugoContentDir, jestHugoOutputDir)
     const testName = path.basename(path.dirname(hugoTestPath))
 
-    var errors = null
+    let errors = {};
     const errorFile = path.resolve(jestHugoOutputDir, "output.err.json")
     if (fs.existsSync(errorFile)) {
       errors = JSON.parse(fs.readFileSync(errorFile))
